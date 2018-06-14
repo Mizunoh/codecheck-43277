2c43578c0b484ccef435a735ac0a60f4214f943b
mizuno.hiroko <mizuno.hiroko@tis.co.jp>
2018-06-14 15:37:40 +0900

    調査用

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -8,7 +8,6 @@ import java.util.Map;
 public class App {
 	public static void main(String[] args) {
 		for (int i = 0, l = args.length; i < l; i++) {
-			System.out.println(args[i]);
 			String output = String.format("argv[%s]: %s", i, args[i]);
 			System.out.println(output);
 			call(args[i]);
@@ -18,19 +17,23 @@ public class App {
 	public static void call(String q){
 		try{
 			URL url = new URL( "http://challenge-server.code-check.io/api/hash");
+			System.out.println("1");
 
 			HttpURLConnection connection = null;
+			System.out.println("2");
 
 			try {
 				connection = (HttpURLConnection) url.openConnection();
 				connection.setRequestMethod("GET");
 				connection.setRequestProperty("q", q);
 				connection.setRequestProperty("hash",null);
+				System.out.println("3");
 
 				connection.connect();
+				System.out.println("4");
 
 				if(connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
-					//String hash;
+					System.out.println("5");
 					Map headers = connection.getHeaderFields();
 					String hash = headers.get("hash").toString();
 					System.out.println(hash);
