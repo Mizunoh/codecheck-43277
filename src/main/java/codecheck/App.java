9be9de8272dbbec2b2f0f2d320a45f93af6bf9a7
mizuno.hiroko <mizuno.hiroko@tis.co.jp>
2018-06-18 15:51:45 +0900

    調査用

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -10,6 +10,7 @@ import java.net.URL;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.core.type.TypeReference;
 
 public class App {
 	public static void main(String[] args) {
