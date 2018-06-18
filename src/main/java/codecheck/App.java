675660fb91950a625bf3de70a465ea6b66760bb7
mizuno.hiroko <mizuno.hiroko@tis.co.jp>
2018-06-18 16:01:36 +0900

    整形

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -15,8 +15,6 @@ import com.fasterxml.jackson.core.type.TypeReference;
 public class App {
 	public static void main(String[] args) {
 		for (int i = 0, l = args.length; i < l; i++) {
-			String output = String.format("argv[%s]: %s", i, args[i]);
-//			System.out.println(output);
 			call(args[i]);
 		}
 	}
@@ -25,23 +23,13 @@ public class App {
 		try {
 			String sUrl = "http://challenge-server.code-check.io/api/hash?q=";
 			URL url = new URL(sUrl + q);
-//			System.out.println("1");
-
 			HttpURLConnection connection = null;
-//			System.out.println("2");
 
 			try {
 				connection = (HttpURLConnection) url.openConnection();
 				connection.setRequestMethod("GET");
-				//System.out.println("3");
-//				System.out.println(q);
-
 				connection.connect();
-//				System.out.println("4");
-//				System.out.println(connection.getResponseCode());
 
-
-				//ここでレスポンスのボディからhashの値を取りたい
 				InputStream is = connection.getInputStream();
 				BufferedReader reader = new BufferedReader(new InputStreamReader(is, "utf-8"));
 				StringBuilder sbBody = new StringBuilder();
@@ -60,44 +48,6 @@ public class App {
 
 				System.out.println(map.get("hash"));
 
-
-
-
-
-
-
-
-				//System.out.println(sbBody.toString());
-
-				/*
-				// ヘッダーの取り方だった
-				LinkedHashMap<String, String> resHeader = new LinkedHashMap<String, String>();
-				Map<String, List<String>> header = connection.getHeaderFields();
-				Iterator<String> headerIt = header.keySet().iterator();
-
-				while (headerIt.hasNext()) {
-					String key = headerIt.next();
-					List<String> valList = header.get(key);
-					if (key != null) {
-						StringBuilder sb = new StringBuilder();
-						for (String val : valList) {
-							if (sb.length() > 0)
-								sb.append("\n");
-							sb.append(val);
-						}
-						resHeader.put(key, sb.toString());
-					}
-				}
-				*/
-
-
-
-				//InputStream is = connection.getInputStream();
-				//BufferedReader reader = new BufferedReader(new InputStreamReader(is, "utf-8"));
-
-				//String hash = getResponse(connection);
-				//System.out.println(hash);
-
 			} finally {
 				if (connection != null) {
 					connection.disconnect();
@@ -107,24 +57,4 @@ public class App {
 			e.printStackTrace();
 		}
 	}
-
-/*	private  static  String getResponse(HttpURLConnection connection) {
-
-	//				if(connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
-					//Map headers = connection.getHeaderFields();
-				   //String hash = headers.get("hash").toString();
-				InputStream is = connection.getInputStream();
-				BufferedReader reader = new BufferedReader(new InputStreamReader(is, "utf-8"));
-				StringBuffer sbBody = new StringBuffer();
-				String s;
-				while ((s = reader.readLine()) != null) {
-					sbBody.append(s);
-					sbBody.append("\n");
-				}
-
-
-//				}
-
-	}*/
-
 }
